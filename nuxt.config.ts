615c923bcadbcba014d475f3c9c13cbfccbda298
thezumrad <thezumrad@gmail.com>
2023-05-04 17:16:30 +0400

    i18n added 2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    "@nuxtjs/prismic",
-    '@nuxtjs/i18n',
+     "@nuxtjs/i18n",
+    "@nuxtjs/prismic"
   ],
   i18n: {
     strategy: "prefix_except_default",
