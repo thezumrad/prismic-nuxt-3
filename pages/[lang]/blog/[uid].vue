3a92a0a06d43831ea1733c74d599b9fa92a00b58
thezumrad <thezumrad@gmail.com>
2023-05-04 20:13:55 +0400

    loading setTimeout

--- pages/[lang]/blog/[uid].vue
+++ pages/[lang]/blog/[uid].vue
@@ -2,22 +2,31 @@
 const { client } = usePrismic();
 const route = useRoute();
 
-const { data: post } = await useAsyncData("post", async () => {
-  const document = await client.getByUID("blog", route.params.uid);
+    const post = ref(null);
+    onMounted(async () => {
+      setTimeout(async () => {
+        try {
+          const document = await client.getByUID('blog', route.params.uid);
 
-  if (document) {
-    return document;
-  } else {
-    throw createError({ statusCode: 404, message: "Page not found" });
-  }
-});
-console.log(post.value.uid,  route.params.uid)
-</script>>
+          if (document) {
+            post.value = document;
+          } else {
+            throw createError({ statusCode: 404, message: 'Page not found' });
+          }
+        } catch (error) {
+          console.error(error);
+          // Handle error
+        }
+      }, 1000); // Wait for 1 second before making the API request
+    });
+
+</script>
 
 <template>
-  <div>
+  <div v-if="post !== null">
     <h1>
       {{ post.data.title[0].text }}
     </h1>
   </div>
+  <div v-else>loading</div>
 </template>
