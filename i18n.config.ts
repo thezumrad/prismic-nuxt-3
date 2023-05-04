3b18f6599b40531fbefd88d34bc80805097e9322
thezumrad <thezumrad@gmail.com>
2023-05-04 16:43:27 +0400

    i18n added

--- i18n.config.ts
+++ i18n.config.ts
@@ -3,22 +3,22 @@ export default defineI18nConfig(nuxt => ({
   locale: 'en',
   messages: {
     en: {
-      home_title: "Home",
-      home_btn: "Go to Home",
-      about_title: "About",
-      about_btn: "Go to About"
+      home_title: 'Home',
+      home_btn: 'Go to Home',
+      about_title: 'About',
+      about_btn: 'Go to About'
     },
     it: {
-      home_title: "Home",
-      home_btn: "Vai alla Home",
-      about_title: "Chi siamo",
-      about_btn: "Vai a Chi siamo"
+      home_title: 'Home',
+      home_btn: 'Vai alla Home',
+      about_title: 'Chi siamo',
+      about_btn: 'Vai a Chi siamo'
     },
     az: {
-      home_title: "Əsas səhifə",
-      home_btn: "Əsas səhifəyə get",
-      about_title: "Haqqımızda",
-      about_btn: "Haqqımızda"
+      home_title: 'Əsas səhifə',
+      home_btn: 'Əsas səhifəyə get',
+      about_title: 'Haqqımızda',
+      about_btn: 'Haqqımızda'
     }
   }
 }))
